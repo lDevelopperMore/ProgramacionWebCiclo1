cea6bc8ecaa389ffdfc65f2053281d1871f39482
unknown <steven.moreno@pi.edu.co>
2026-04-10 23:25:00 -0500

    ajustamos scripts en carpeta agregamos menus y subimos en vercel corregimos direccionamiento de carpetas

---
+++ STATIC/JS/script.js
@@ -0,0 +1,34 @@
+/* Función para el menú de navegación (Hamburguesa) */
+function controlSemanas() {
+    // Buscamos específicamente tu menú de arriba
+    var x = document.getElementById("navSemanas");
+
+    if (x.className === "topnav") {
+        x.className += " responsive"; // Lo abre
+    } else {
+        x.className = "topnav"; // Lo cierra
+    }
+}
+
+/* LA FUNCIÓN QUE LE EDITARON Y SE DAÑÓ: */
+function funcionFormulario(elemento) {
+    let color_bg = elemento.querySelector("input[type='text']").value;
+    document.querySelector("body").style.background = color_bg;
+}
+const form = document.querySelector('form');
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+});
+/* Función para el menú de navegación (Hamburguesa) del ejercicio práctico de la semana 7 */
+                function activarEjercicio() {
+                    // Buscamos el ID exacto que le pusiste al div: "ejercicioW3"
+                    var x = document.getElementById("ejercicioW3");
+
+                    // Si la clase es la normal, le añadimos " responsive" (con el espacio)
+                    if (x.className === "topnav-ejercicio") {
+                        x.className += " responsive";
+                    } else {
+                        // Si ya la tiene, se la quitamos para cerrar el menú
+                        x.className = "topnav-ejercicio";
+                    }
+                }
